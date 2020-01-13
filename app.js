542df11ffb44672ee605121b40426cd0a4be5b78
asiamorozova <57855398+asiamorozova@users.noreply.github.com>
2020-01-13 15:15:59 -0800

    added recent chages

---
+++ app.js
@@ -0,0 +1,15 @@
+const noun = document.getElementById('noun');
+const nounOutput = noun.value
+
+
+
+
+const paragraph = document.getElementById('paragraph');
+paragraph.textContent = 'nounInput'
+
+
+const button = document.getElementById('lib-button');
+button.addEventListener('click',() => {
+git status
+
+}
